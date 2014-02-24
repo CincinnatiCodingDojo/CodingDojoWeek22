30e87430a0ea9619caa240eb60861d3dcc6985b0
Bobby Eickhoff <bobby.eickhoff2@kroger.com>
2014-02-24 12:28:48 -0500

    commit working solution

--- src/euler/core.clj
+++ src/euler/core.clj
@@ -1,7 +1,18 @@
 (ns euler.core
   (:gen-class))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(defn pairs-of-three-digit-numbers []
+  (for [x (range 100 1000) y (range 100 1000)] [x y]))
+
+(defn product [[x y]]
+  (* x y))
+
+(defn palindrome? [number]
+  (= (seq (str number)) (reverse (seq (str number))))
+  )
+
+(defn euler-4 []
+  (->> (pairs-of-three-digit-numbers)
+       (map product)
+       (filter palindrome?)
+       (apply max)))
--- test/euler/core_test.clj
+++ test/euler/core_test.clj
@@ -1,3 +1,21 @@
 (ns euler.core-test
   (:require [midje.sweet :refer :all]
             [euler.core :refer :all]))
+
+(fact "should generate all three digit pairs"
+  (first (pairs-of-three-digit-numbers)) => [100,100]
+  (last (pairs-of-three-digit-numbers)) => [999,999]
+  (count (pairs-of-three-digit-numbers)) => (* 900 900))
+
+(fact "should create a product from a pair of 2 three digit numbers"
+  (product [100,100]) => 10000
+  (product [200 200]) => 40000 )
+
+(fact "palindrome? should tell whether a number is a palindrome"
+  (palindrome? 1001) => true
+  (palindrome? 1002) => false
+  )
+
+
+(fact "the solution for Project Euler problem #4 is _________"
+  (euler-4) => 906609)
